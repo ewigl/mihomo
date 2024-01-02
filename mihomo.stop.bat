b88ce67c2560023093967f72bb3a52a872a0b380
ewigl <ewiglichtl@gmail.com>
2024-01-02 21:06:33 +0800

    add stop bat

---
+++ mihomo.stop.bat
@@ -0,0 +1,26 @@
+@echo off
+
+SET PROCESS_NAME=mihomo-windows-amd64.exe
+
+echo Stop Mihomo ?
+pause
+
+tasklist | findstr "%PROCESS_NAME%"
+
+if ERRORLEVEL 1 (
+    echo Mihomo not running.
+) else (
+    taskkill /F /T /im %PROCESS_NAME%
+)
+
+timeout /t 2
+
+tasklist | findstr "%PROCESS_NAME%"
+
+if ERRORLEVEL 1 (
+    echo Mihomo stoped.
+) else (
+    echo Error occurred, pls try again with admin permission.
+)
+
+pause
